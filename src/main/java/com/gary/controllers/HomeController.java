6d92f966f0e6feae506c6c37fc6aaed7912e2ad5
Gary Lai <garylai1990@gmail.com>
2018-04-03 11:41:36 -0400

    :sparkles: (controllers) index page

---
+++ src/main/java/com/gary/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.gary.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ResponseEntity<String> get() {
+        return new ResponseEntity<>("Hello World!", HttpStatus.OK);
+    }
+}
